4dd4aaef4f27af9bd2e265825c3397513818438c
guanyanliang <guanyanliangbyx@163.com>
2016-03-23 08:59:02 +0800

    zm

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 class  My_Class_One {
     public function oneMethod() {
-            echo '第一个方法';        
+            
     }
     
     public function twoMethod() {
-            echo '第二个方法';
+           
     }
 }
 ?>
