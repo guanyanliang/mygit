e7864bb346941bc49f7d093452cc9291a1f47510
guanyanliang <guanyanliang@sina.com>
2016-03-24 11:38:47 +0800

    u

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 class  My_Class_One {
     public function oneMethod() {
-            
+            echo "one";
     }
     
     public function twoMethod() {
-           
+           echo "abc";
     }
 }
 ?>
